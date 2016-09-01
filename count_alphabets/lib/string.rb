ee217089da94af527dc80eade1d0a0c36905141b
ganisivakumar <ganisivakumar@gmail.com>
2016-09-01 20:57:41 +0530

    Add files for review

--- count_alphabets/lib/string.rb
+++ count_alphabets/lib/string.rb
@@ -1,11 +1,11 @@
 class String
   def count_alphabets
-    counts = Hash.new(0)
+    hash_count_alphabets = Hash.new(0)
     #Regex pattern for removing non-alphabets
     downcase.gsub(/[^a-z]/, "").each_char do |char| 
-      counts[char] += 1
+      hash_count_alphabets[char] += 1
     end
-    counts
+    hash_count_alphabets
   end
 end
   
