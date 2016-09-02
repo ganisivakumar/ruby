9aafe69e2a76f9d87c9be66509f479ea0378a0a5
ganisivakumar <ganisivakumar@gmail.com>
2016-09-02 18:40:24 +0530

    Add files for review

---
+++ hash_odd_even/bin/main.rb
@@ -0,0 +1,4 @@
+require_relative "../lib/array"
+
+p ["siva",123,"fd",45].group_by_type
+
---
+++ hash_odd_even/lib/array.rb
@@ -0,0 +1,19 @@
+class Array
+  
+  def group_by_type
+    hash_group_by_size = Hash.new { |hash, key| hash[key] = [] }
+    hash_group_by_type = Hash.new { |hash, key| hash[key] = [] }
+    hash_group_by_size = group_by { |element| element.to_s.length }
+
+    hash_group_by_size.inject(0) do |result, (key, value)|
+      if key.odd?
+        hash_group_by_type["odd"] << value
+      else
+        hash_group_by_type["even"] << value
+      end
+      hash_group_by_type
+    end
+  end
+
+end
+
