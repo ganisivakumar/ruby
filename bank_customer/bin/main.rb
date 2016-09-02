ae4810e8f13e369337310dd663e6fdf49476fdae
ganisivakumar <ganisivakumar@gmail.com>
2016-09-02 15:16:06 +0530

    Add files for review

--- bank_customer/bin/main.rb
+++ bank_customer/bin/main.rb
@@ -1,12 +1,12 @@
 require_relative "../lib/customer"
 loop do
   puts 'Please enter customer name or q/Q to quit:'
-  input = gets.chomp
-    if input == 'q' or input == 'Q'
-      break
-    end
-  customer1 = Customer.new(input)
-  puts customer1
+  customer_name = gets.chomp
+  if customer_name == 'q' or customer_name == 'Q'
+    break
+  end
+  customer = Customer.new(customer_name)
+  puts customer
   puts 'enter 1 to deposit or 2 to withdraw or 0 to quit'
   option = gets.chomp.to_i
   if option == 0
@@ -14,13 +14,13 @@ loop do
   elsif option == 1
     puts 'Enter an amount to deposit'
     deposit = gets.chomp.to_i
-    customer1.deposit(deposit)
-    puts "Balance : #{ customer1.balance }"
+    customer.deposit(deposit)
+    puts "Balance : #{ customer.balance }"
   elsif option == 2
     puts 'Enter an amount to withdraw'
-    deposit = gets.chomp.to_i
-    customer1.withdraw(deposit)
-    puts "Balance : #{ customer1.balance }"
+    withdraw = gets.chomp.to_i
+    customer.withdraw(withdraw)
+    puts "Balance : #{ customer.balance }"
   end
 end
 
