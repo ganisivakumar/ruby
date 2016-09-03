978c7f9c5eb2535fc6b91b5d09b7a2fe6b56f592
ganisivakumar <ganisivakumar@gmail.com>
2016-09-03 17:25:38 +0530

    Add files for review

--- group_by_length/lib/array.rb
+++ group_by_length/lib/array.rb
@@ -3,7 +3,7 @@ class Array
   def group_by_length
     hash_group_by_length = Hash.new{ |hash,key| hash[key] = [] }
     for element in self do
-      hash_group_by_length[element.to_s.size] << element
+      hash_group_by_length[element.to_s.length] << element
     end
     hash_group_by_length
   end
