ff8d843d70383b7ae64ec7f784bc6431b29b61e6
ganisivakumar <ganisivakumar@gmail.com>
2016-09-06 09:49:10 +0530

    Add files for review

--- interest/bin/main.rb
+++ interest/bin/main.rb
@@ -4,6 +4,6 @@ principal = gets.to_f
 puts 'Enter number of years to calculate interest:'
 time = gets.to_f
 
-interest = Interest.new(principal,time)
+interest = Interest.new(principal, time)
 puts "Difference in interest is : #{interest.difference} "
 
