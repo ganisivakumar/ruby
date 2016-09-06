cec7a7be1a24c302243235de3534e38bde195cc0
ganisivakumar <ganisivakumar@gmail.com>
2016-09-06 15:44:13 +0530

    Add files for review

--- interest/lib/interest.rb
+++ interest/lib/interest.rb
@@ -5,10 +5,16 @@ class Interest
     @time = time
     @rate = 10
   end
+
+  def simple_interest
+    (@principal * @time * @rate)/100.0
+  end
+
+  def compound_interest
+    @principal * ((1 + (@rate/100.0))**@time)
+  end
   
   def difference
-    simple_interest = (@principal * @time * @rate)/100.0
-    compound_interest = @principal * ((1 + (@rate/100.0))**@time)
     (compound_interest - simple_interest).round(2)
   end
 
