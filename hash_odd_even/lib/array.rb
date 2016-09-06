c9023edd2fe42f4fdc072e01a8a75946bc5ee4a1
ganisivakumar <ganisivakumar@gmail.com>
2016-09-06 15:54:08 +0530

    Add files for review

--- hash_odd_even/lib/array.rb
+++ hash_odd_even/lib/array.rb
@@ -1,11 +1,13 @@
 class Array
   
-  def group_by_type
+  def group_by_size
     hash_group_by_size = Hash.new { |hash, key| hash[key] = [] }
-    hash_group_by_type = Hash.new { |hash, key| hash[key] = [] }
     hash_group_by_size = group_by { |element| element.to_s.length }
+  end
 
-    hash_group_by_size.inject(0) do |result, (key, value)|
+  def group_by_type
+    hash_group_by_type = Hash.new { |hash, key| hash[key] = [] }
+    group_by_size.inject(0) do |result, (key, value)|
       if key.odd?
         hash_group_by_type["odd"] << value
       else
