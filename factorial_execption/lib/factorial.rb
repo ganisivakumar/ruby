26166d6a4ddba4b5eeb871e45fd9db3b2f726805
ganisivakumar <ganisivakumar@gmail.com>
2016-09-06 09:33:41 +0530

    Add files for review

--- factorial_execption/lib/factorial.rb
+++ factorial_execption/lib/factorial.rb
@@ -3,7 +3,7 @@ class Factorial
   def calculate(num)
     factorial = 1
     
-    if !n.match(/^\d+$/) 
+    if !num.match(/^\d+$/) 
       raise RuntimeError, "input not a positive integer"
     else 
       digit_range = Range.new(1, num.to_i)
